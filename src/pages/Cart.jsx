4cfcf7ede7d3cb55c71735eeccb13df4068da872
Vaibhav Nathe <vaibhavnathe2002@gmail.com>
2024-05-19 01:40:36 +0530

    Pages Creation - Cart & Home pages are created

---
+++ src/pages/Cart.jsx
@@ -0,0 +1,61 @@
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import CartItem from "../components/CartItem";
+
+const Cart = () => {
+
+  const {cart} = useSelector((state) => state);
+  const [totalAmount, setTotalAmount] = useState(0);
+
+  console.log("printing cart : ");
+  console.log(cart);
+
+  useEffect(() => {
+    setTotalAmount(cart.reduce((acc, curr) => acc + curr.price ,0));
+  },[cart]);
+
+  return (
+    <div>
+      {
+        cart.length > 0 ?
+        (<div className="flex max-w-6xl">
+
+          <div>
+            {
+              cart.map((item, index) => {
+                return <CartItem key={item.id} item={item} itemIndex={index}/>
+              })
+            }
+          </div>
+
+          <div className="flex flex-col">
+
+            <div>
+              <div>Your Cart</div>
+              <div>Summary</div>
+              <p>
+                <span>Total Items : {cart.length}</span>
+              </p>
+            </div>
+
+            <div>
+              <p>Total Amount : ${totalAmount}</p>
+              <button>CheckOut Now</button>
+            </div>
+
+          </div>
+
+        </div>): 
+        (<div>
+          <h1>Your Cart is Empty!</h1>
+          <Link to="/">
+              <button>Shop Now</button>
+          </Link>
+        </div>)
+      }
+    </div>
+  );
+};
+
+export default Cart;
